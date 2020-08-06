c940e60a3cec1fedd89f190790232a4f4e304ec2
gurpreet8033 <uic.19mca8033@gmail.com>
2020-08-06 19:26:02 +0530

    done!

---
+++ app.js
@@ -0,0 +1,11 @@
+(function() {
+
+    angular.module('guri',[])
+    .controller('guri_c',function($scope){
+        $scope.name = "Tonight i will fuck you";
+        $scope.name2 = "raiden";
+        $scope.age=20;
+    });
+    
+
+})();
